b0aa1621e930614063d3e5be716f1b2c7d9ee4e1
Dokky <107966608+dokky41@users.noreply.github.com>
2023-06-27 21:46:42 +0900

    (23.6.27) 스마트포인터

---
+++ Solution/(2023.6.26)업캐스팅.h
@@ -0,0 +1,33 @@
+#pragma once
+//#include <iostream>
+#include "Marine.h"
+#include "Ghost.h"
+
+using namespace std; 
+
+void Recovery(Unit * unit)
+{
+	unit->SetHP(200);
+}
+
+int main()
+{
+	// 업 캐스팅
+	Marine marine; // 체력 100
+	Ghost ghost; // 체력 50
+
+	//Unit * uptr = &marine;
+	//uptr->Skill();
+	//
+	//uptr = &ghost;
+	//uptr->Skill();
+
+	Recovery(&marine);  // 체력 200
+	Recovery(&ghost);  // 체력 200
+
+	cout << "마린 체력 : " << marine.GetHP() << endl;
+	cout << "고스트 체력 : " << ghost.GetHP() << endl;
+
+
+	return 0;
+}
---
+++ Solution/Item.cpp
@@ -0,0 +1,11 @@
+#include "Item.h"
+
+Item::Item()
+{
+	std::cout << "Item »ý¼º" << std::endl;
+}
+
+Item::~Item()
+{
+	std::cout << "Item ¼Ò¸ê" << std::endl;
+}
